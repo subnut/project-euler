f846a5ea2fc5cc39d491cfadfdf9d272f7dc88a5
Subhaditya Nath <sn03.general@gmail.com>
2023-06-22 13:47:52 +0530

    Update pymods.generators

--- pymods/generators.py
+++ pymods/generators.py
@@ -2,24 +2,10 @@
 Provides generators for common mathematical sequences
 """
 
-import typing as t
-
-def primegen() -> t.Generator:
-    """Generator of prime numbers"""
-    dictionary: t.Dict[int, int] = {}
-    value: int = 2
-    while True:
-        if value not in dictionary.values():
-            dictionary[value] = value ** 2
-            yield value
-        else:
-            for (key, val) in dictionary.items():
-                if val == value:
-                    dictionary[key] = val + key
-        value += 1
+import typing as _t
 
 
-def fibgen() -> t.Generator:
+def fibgen() -> _t.Generator:
     """Generator of Fibonacci numbers"""
     prev = 0
     now = 1
@@ -28,3 +14,41 @@ def fibgen() -> t.Generator:
         prev = now
         now = next
         yield now
+
+
+def primegen() -> _t.Generator:
+    """Generator of prime numbers"""
+    dictionary: _t.Dict[int, int] = {}
+    value: int = 2
+    while True:
+        if value not in dictionary.values():
+            dictionary[value] = value**2
+            yield value
+        else:
+            for key, val in dictionary.items():
+                if val == value:
+                    dictionary[key] = val + key
+        value += 1
+
+
+class _DEPRECATED:
+    """
+    Generators that have been deprecated for various reasons.
+    Some of them have been replaced by newer versions that are faster.
+    """
+
+    def primegen() -> _t.Generator:
+        """Generator of prime numbers (SLOW)"""
+        keys: _t.List[int] = []
+        vals: _t.List[int] = []
+        value: int = 2
+        while True:
+            if value not in vals:
+                keys.append(value)
+                vals.append(value)
+                yield value
+            else:
+                for i in range(len(vals)):
+                    if vals[i] == value:
+                        vals[i] += keys[i]
+            value += 1
