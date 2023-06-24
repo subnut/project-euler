af195b10a59beddc5c831421c014b9acc5b54a49
Subhaditya Nath <sn03.general@gmail.com>
2023-06-24 19:47:30 +0530

    [UPD] 50 - D
    The program runs and outputs the longest sequence as and when it finds
    it, but then hangs till it has checked all of the remaining
    possibilities.
    
    It outputs the correct value almost instantly after the prime generation
    step, so I see that as a win.

--- 50.d
+++ 50.d
@@ -16,61 +16,46 @@ void main() {
 
 	writeln("Looking for combinations...");
 	auto tpool = taskPool();
-	ulong[][][ulong] dict;
-	tpool.put(task!find(primes, &dict, 0));
+	ulong curmax = 183;
+	ulong curstart;
 	foreach (i; 0 .. primes.length)
-		tpool.put(task!find_skipped(primes, &dict, i));
+		tpool.put(task!find(primes, i, &curmax, &curstart));
 	tpool.finish(true);
 	writeln("Found all combinations.");
 
+
+	writeln();
+	writeln(curmax, " terms");
 	ulong max;
-	ulong max_num;
-	ulong[] max_comb;
-	writeln("Finding longest combination...");
-	foreach (num, combos; dict)
-		foreach (combo; combos)
-			if (combo.length > max) {
-				max = combo.length;
-				max_num = num;
-				max_comb = combo;
-			}
-
-	writeln(max_num, " = ", max_comb);
+	foreach (n; primes[curstart .. curstart + curmax])
+		max += n;
+	writeln(max, " = ", primes[curstart .. curstart + curmax]);
 }
 
-void find(ulong[] primes, ulong[][][ulong] *dict, ulong i) {
-	ulong sum = primes[i];
-	foreach (j; i+1 .. primes.length) {
-		sum += primes[j];
-		foreach (n; primes[j .. $]) {
-			if (n > sum)
-				break;
-			if (n == sum) {
-				ulong[] arr;
-				foreach (k; primes[i .. j+1]) arr ~= k;
-				synchronized { (*dict)[n] ~= arr; }
-				break;
-			}
-		}
-	}
-}
-
-// We're gonna be fine if the part we're searching doesn't contain 2
+// skipping is fine if the sequence doesn't start with 2
 // This works because odd + odd = even, which isn't prime anyways.
 // So we skip odd+odd and go directly to odd+odd+odd
-void find_skipped(ulong[] primes, ulong[][][ulong] *dict, ulong i) {
-	ulong sum = primes[i];
-	for (auto j = i+1; j < primes.length; j++) {
-		sum += primes[j++];	// Here's the skipping
+void find(const ulong[] primes, ulong start, ulong *curmax, ulong *curstart) {
+	ulong sum;
+	ulong saved_max = *curmax;
+	foreach (n; primes[start .. saved_max + start])
+		sum += n;
+	for (auto j = start + saved_max; j < primes.length; j++) {
 		sum += primes[j];
 		foreach (n; primes[j .. $]) {
-			if (n > sum)
-				break;
+			if (n > sum) break;
 			if (n == sum) {
-				ulong[] arr;
-				foreach (k; primes[i .. j+1]) arr ~= k;
-				synchronized { (*dict)[n] ~= arr; }
-				break;
+				auto len = j - start + 1;
+				if (len > *curmax) {
+					synchronized {
+						if (len > *curmax) {
+							*curmax = len;
+							*curstart = start;
+						}
+					}
+					writeln(sum, "\t", len, "\t", primes[start]);
+					break;
+				}
 			}
 		}
 	}
