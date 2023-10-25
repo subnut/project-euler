4331c3ed3b11a5352b8ce87d4f1266625afd29a8
Subhaditya Nath <sn03.general@gmail.com>
2023-10-25 18:24:19 +0530

    [UPD] 10 - D

--- 10.d
+++ 10.d
@@ -4,8 +4,7 @@ import dmods.generators : Primegen;
 void main() {
 	Primegen gen;
 	ulong sum = 0;
-	ulong next;
-	while ((next = gen.next()) < 2_000_000)
-		sum+= next;
+	for (ulong next; next < 2_000_000; next = gen.next())
+		sum += next;
 	writeln(sum);
 }
