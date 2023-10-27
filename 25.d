38b5adca34f761d21042dfcd0be86b5be0afb7d3
Subhaditya Nath <sn03.general@gmail.com>
2023-10-27 12:42:55 +0530

    [UPD] 25 - D

--- 25.d
+++ 25.d
@@ -3,14 +3,15 @@ import std.stdio : writeln;
 
 class MyRange : FibRangeInf {
 	override bool empty() {
-		import std.bigint : BigInt;
-		return now >= BigInt(10) ^^ 999;
+		return now >= _T(10) ^^ 999;
 	}
 }
 
 void main() {
 	long sum = 0;
-	foreach(n; new MyRange) sum++;
+	foreach(_; new MyRange)
+		sum++;
+
 	writeln(sum + 1);
 	// +1 cause sum has counted the no. of terms that have less than 1000 digits
 	// therefore, the index of the 1000-digit fibonacci number is sum+1
